1c5259ab9a451d33cca130abb962e2710c740b5c
yuelicmu <yuelipku1996@gmail.com>
2017-09-07 13:12:25 -0400

    fix some logical mistakes

--- nnk.r
+++ nnk.r
@@ -6,32 +6,29 @@ for(i in 1:nrow(X))  # for every point:
 {
     x <- sum( (X[i,]-y)^2 )
     k = 1  # insert
-    while(x > d[k] && k <= n)
-        {
-          k <- k + 1  
-        }
-    if(k == 1) {  # watch the boundaries
-      d <- c(x, d[-n])
-      w <- c(i, w[-n])
+    while(x > d[k] && k <= n){
+      if(k == 1) {  # watch the boundaries
+        d <- c(x, d[-n])
+        w <- c(i, w[-n])
+      }
+      else if (  k < n  ) {
+        d <- c(d[1:(k-1)], x, d[k:(n-1)])
+        w <- c(w[1:(k-1)], i, w[k:(n-1)])
+      } 
+      else {
+        d <- c(d[-n], x)
+        w <- c(w[-n], i)
+      }
     }
-    else if (  k < n  ) 
-    {
-      d = c(d[1:(k-1)], x, d[k:(n-1)])
-      w = c(w[1:(k-1)], i, w[k:(n-1)])
-    }
-    else if ( k == n) {
-      d <- c(d[-n], x)
-      w <- c(w[-n], i)
-    }
+    k <- k + 1  
 }
-
 print(c(w[n], d[n], X[w[n],]))
 
 # Try
-# X <- matrix(1:16,8,2)
-# y <- c(5, 14)
-# n <- 3
-# source("nnk.r")
+X <- matrix(1:16,8,2)
+y <- c(5, 14)
+n <- 3
+source("nnk.r")
 
 
 
