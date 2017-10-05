2677cc0fa2412c1ab977b654ed26afcc3367affb
yuelicmu <yuelipku1996@gmail.com>
2017-10-05 15:26:18 -0400

    nothing

--- maze-solver/solver.py
+++
@@ -1,7 +0,0 @@
-from Maze import MazeSolver
-Solver = MazeSolver()
-maze = Solver.mazereading('maze2.txt')
-adjacancy_dict = Solver.adjacancyDict(maze)
-maze_info = Solver.Checking(maze)
-path = Solver.findpath(adjacancy_dict,maze_info)
-Solver.mazeprint(maze,path)
